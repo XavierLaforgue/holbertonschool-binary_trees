829074d7acbc7d4d7153922e88e5b4d6e03193f7
XavierLaforgue <xavierjimmys@gmail.com>
2025-04-10 01:52:39 +0200

    6-binary_tree_preorder.c update
    Executes only if both the node and the function are non-NULL

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -9,12 +9,12 @@
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree != NULL)
+	if (tree && func)
 	{
 		func(tree->n);
-		if (tree->left != NULL)
+		if (tree->left)
 			binary_tree_preorder(tree->left, func);
-		if (tree->right != NULL)
+		if (tree->right)
 			binary_tree_preorder(tree->right, func);
 	}
 }
--- 6-main.c
+++ 6-main.c
@@ -27,7 +27,9 @@ int main(void)
 	root->left->left = binary_tree_node(root->left, 6);
 	root->left->right = binary_tree_node(root->left, 56);
 	root->right->left = binary_tree_node(root->right, 256);
+	root->right->left->right = binary_tree_node(root->right->left, 2056);
 	root->right->right = binary_tree_node(root->right, 512);
+	root->right->right->left = binary_tree_node(root->right->right, 1024);
 
 	binary_tree_print(root);
 	binary_tree_preorder(root, &print_num);
